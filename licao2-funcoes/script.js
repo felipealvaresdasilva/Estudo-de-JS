2509131a90a5218396a0e2f6347b5f91eec7b6c2
Felipe <felipealvaresdasilva@gmail.com>
2026-06-17 21:50:12 -0300

    feat: aula 2 - criação de funçoes

---
+++ licao2-funcoes/script.js
@@ -0,0 +1,12 @@
+function soma(x, y) {
+    let result = x + y;
+    console.log('O resultado de X+Y é: ' + result);
+}
+
+soma(10,20);
+
+function nomeCompleto (nome, sobrenome) {
+    console.log(`O nome completo do indivíduo é ${nome} ${sobrenome}`);
+}
+
+nomeCompleto('Felipe', 'Alvares');
