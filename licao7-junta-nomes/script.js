99b29dc29c80edc616b383db686fe3b22c5abe7a
Felipe <felipealvaresdasilva@gmail.com>
2026-07-06 22:34:12 -0300

    feat: aula 7

---
+++ licao7-junta-nomes/script.js
@@ -0,0 +1,12 @@
+let Nome = document.getElementById('nome');
+let Sobrenome = document.getElementById('sobrenome');
+let botao = document.getElementById('juntar');
+let result = document.getElementById('resultado');
+
+function juntaNomes(valor1, valor2) {
+    return `O nome da pessoa é ${valor1} ${valor2}`;
+}
+
+botao.addEventListener('click', function () {
+    result.innerHTML = juntaNomes(Nome.value, Sobrenome.value);
+});
